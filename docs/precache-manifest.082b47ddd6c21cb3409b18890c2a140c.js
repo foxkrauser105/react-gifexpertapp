0794b999534ed894ee9a26ec9371c9bbe40ec175
Caesar Carrillo <cesar.alejandrocarrillo7@hotmail.com>
2021-07-10 10:52:22 -0600

    docs(build) agregado

---
+++ docs/precache-manifest.082b47ddd6c21cb3409b18890c2a140c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fee6f92b6b0f97dd45012b17e02d536a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "858363af136983cd5e27",
+    "url": "/static/css/main.08dc6e5e.chunk.css"
+  },
+  {
+    "revision": "21128ddbf1ff63946978",
+    "url": "/static/js/2.31a10122.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/static/js/2.31a10122.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "858363af136983cd5e27",
+    "url": "/static/js/main.a030c362.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
